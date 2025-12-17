7719c2276ab64e903e2055cadac15f9db17b0d49
MarinZ-boop <marinzoric29@gmail.com>
2025-12-17 17:58:34 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,18 +1,16 @@
+const provider = new firebase.auth.GoogleAuthProvider();
+
 function login() {
-  const provider = new firebase.auth.GoogleAuthProvider();
-
-  // Dodaj tvoj Client ID (nije obavezno ako već radiš preko Firebase web configa,
-  // ali ponekad pomaže za GitHub Pages i custom domena)
-  provider.setCustomParameters({
-    client_id: "101963018302-j9ter46s7eakbrs5d0kiuk0s66k31uql.apps.googleusercontent.com"
-  });
-
-  auth.signInWithPopup(provider)
-    .then(() => {
-      window.location = "dashboard.html";
-    })
-    .catch(error => {
-      console.error(error.message);
-      alert("Login failed: " + error.message);
-    });
+  auth.signInWithRedirect(provider);
 }
+
+auth.getRedirectResult()
+  .then((result) => {
+    if (result.user) {
+      window.location = "dashboard.html";
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+    alert("Login failed: " + error.message);
+  });
